7be2e4367d982e86cb285ac2e6d9188873959809
piyapaul <palpiya19@gmail.com>
2025-02-18 15:33:51 +0530

    test

---
+++ vitereact/src/App.jsx
@@ -0,0 +1,13 @@
+import Basic from "./basic"
+function App() {
+  return (
+   <>
+   <Basic>
+    <h1>Learning React</h1>
+    <p>after learning javascript</p>
+   </Basic>
+   </>
+  );
+}
+
+export default App
---
+++ vitereact/src/basic.jsx
@@ -0,0 +1,6 @@
+function Basic(){
+    return(
+        <h2>Hello World</h2>
+    );
+}
+export default Basic
